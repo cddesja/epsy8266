3993e9af24785d84e5ca90817ee676db6289c64b
Chris Desjardins <cddesjardins@gmail.com>
2019-03-28 15:56:54 -0500

    updated script

--- course_materials/scripts/houghton2007.R
+++ course_materials/scripts/houghton2007.R
@@ -2,6 +2,7 @@
 # In this activity, I have given you the data, however, you need to convert it
 # so that lavaan can use it.
 # You then need to fit and estimate the model in lavaan
+library(lavaan)
 N <- 263
 M <- c(3.96, 4.12, 4.13, 3.97, 3.61, 3.3, 2.13, 1.63, 1.99, 3.86, 3.62, 3.5)
 SD <- c(.939, 1.017, .937, .562, .76, .524, .585, .609, .731, .711, 1.124, 1.001)
@@ -20,3 +21,77 @@ lowTri <- "
 .061 .028 -.035 -.058 -.051 -.003 -.04 -.04 -.018 .284 1
 .113 .174 .059 .063 .138 .044 -.119 -.073 -.084 .563 .379 1
 "
+cov.mat <- getCov(lowTri, names = varNames, sds = SD)
+
+# step 1 ----
+one.mod <- "
+fac1 =~ eba + st + mi + das1 + das2 + das3 + jdiw1 + jdiw2 + jdiw3 + uf1 + uf2 + ford
+"
+one.fit <- cfa(mod = one.mod, sample.cov = cov.mat, sample.nobs = N)
+fitmeasures(one.fit, c("chisq", "df", "pvalue", "cfi", "tli", "rmsea", "srmr"))
+
+# step 2 --- 
+meas.mod <- '
+cts =~ eba + st + mi
+dtp =~ das1 + das2 + das3
+js =~ jdiw1 + jdiw2 + jdiw3
+swb =~ uf1 + uf2 + ford
+'
+meas.fit <- cfa(mod = meas.mod, sample.cov = cov.mat, sample.nobs = N)
+anova(meas.fit, one.fit) # discriminant validity
+summary(meas.fit, standardized = TRUE, fit.measures = TRUE)
+lavResiduals(meas.fit, type = "cor")$cov
+lavResiduals(meas.fit, type = "cor")$cov.z
+modificationindices(meas.fit, sort. = TRUE)
+
+# step 3 ----
+sr.mod <- "
+cts =~ eba + st + mi
+dtp =~ das1 + das2 + das3
+js =~ jdiw1 + jdiw2 + jdiw3
+swb =~ uf1 + uf2 + ford
+dtp ~ cts
+swb ~ a*dtp
+js ~ b*swb + c*dtp
+
+# indirect effect
+ab := a*b
+"
+sr.fit <- cfa(mod = sr.mod, sample.cov = cov.mat, sample.nobs = N)
+anova(sr.fit, meas.fit)
+summary(sr.fit, fit.measures = TRUE, standardized = TRUE)
+lavResiduals(sr.fit, type = "cor")$cov
+lavResiduals(sr.fit, type = "cor")$cov.z
+modificationindices(sr.fit, sort. = TRUE)
+
+
+meas.mod2 <- '
+# define latent variables
+cts =~ 1*eba + st + mi
+dtp =~ 1*das1 + das2 + das3
+js =~ 1*jdiw1 + jdiw2 + jdiw3
+swb =~ 1*uf1 + uf2 + ford
+
+# define lv cov/var
+cts ~~ cts + dtp + js + swb
+dtp ~~ dtp + js + swb
+js ~~ js + swb
+swb ~~ swb
+
+# variance of manifest variables
+eba ~~ eba
+st ~~ st
+mi ~~ mi
+das1 ~~ das1
+das2 ~~ das2
+das3 ~~ das3
+jdiw1 ~~ jdiw1
+jdiw2 ~~ jdiw2
+jdiw3 ~~ jdiw3
+uf1 ~~ uf1
+uf2 ~~ uf2
+ford ~~ ford
+'
+meas.fit2 <- lavaan(meas.mod2, sample.cov = cov.mat, sample.nobs = N)
+summary(meas.fit)
+summary(meas.fit2)
